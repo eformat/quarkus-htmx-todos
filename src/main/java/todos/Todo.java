4e45d5352b5ef45a323e853d6d4be397039eef68
Mike Hepburn <eformat@gmail.com>
2022-05-10 12:22:53 +1000

    🐾 formatting 🐾

--- src/main/java/todos/Todo.java
+++ src/main/java/todos/Todo.java
@@ -2,6 +2,7 @@ package todos;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.panache.common.Sort;
+
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.jboss.resteasy.annotations.jaxrs.FormParam;
 
@@ -17,48 +19,48 @@ import org.jboss.resteasy.annotations.jaxrs.FormParam;
 @Table(name = "todos")
 public class Todo extends PanacheEntityBase {
 
-  @Id
-  @GeneratedValue
-  public UUID id;
+    @Id
+    @GeneratedValue
+    public UUID id;
 
-  @FormParam
-  public String title;
+    @FormParam
+    public String title;
 
-  public Boolean completed = Boolean.FALSE;
+    public Boolean completed = Boolean.FALSE;
 
-  @CreationTimestamp
-  @Column(name = "created_timestamp")
-  public Timestamp createdTimestamp;
+    @CreationTimestamp
+    @Column(name = "created_timestamp")
+    public Timestamp createdTimestamp;
 
-  public boolean isNotCompleted() {
-    return !completed;
-  }
+    public boolean isNotCompleted() {
+        return !completed;
+    }
 
-  public Boolean getCompleted() {
-    return completed;
-  }
+    public Boolean getCompleted() {
+        return completed;
+    }
 
-  public static List<Todo> listAll() {
-    return findAll(Sort.ascending("createdTimestamp")).list();
-  }
+    public static List<Todo> listAll() {
+        return findAll(Sort.ascending("createdTimestamp")).list();
+    }
 
-  public static List<Todo> listActive() {
-    return list("completed=false", Sort.ascending("createdTimestamp"));
-  }
+    public static List<Todo> listActive() {
+        return list("completed=false", Sort.ascending("createdTimestamp"));
+    }
 
-  public static List<Todo> listCompleted() {
-    return list("completed=true", Sort.ascending("createdTimestamp"));
-  }
+    public static List<Todo> listCompleted() {
+        return list("completed=true", Sort.ascending("createdTimestamp"));
+    }
 
-  public static void updateAllCompleted(boolean completed) {
-    update("completed = ?1", completed);
-  }
+    public static void updateAllCompleted(boolean completed) {
+        update("completed = ?1", completed);
+    }
 
-  public static void deleteCompleted() {
-    delete("completed = true");
-  }
+    public static void deleteCompleted() {
+        delete("completed = true");
+    }
 
-  public static int countActive() {
-    return (int) count("completed != true");
-  }
+    public static int countActive() {
+        return (int) count("completed != true");
+    }
 }
--- src/main/java/todos/TodoResource.java
+++ src/main/java/todos/TodoResource.java
@@ -2,6 +2,7 @@ package todos;
 
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
+
 import java.util.List;
 import java.util.UUID;
 import javax.transaction.Transactional;
@@ -12,93 +13,94 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
 import org.jboss.resteasy.annotations.Form;
 
 @Path("/todos")
 public class TodoResource {
 
-  @CheckedTemplate
-  public static class Templates {
+    @CheckedTemplate
+    public static class Templates {
 
-    public static native TemplateInstance list(List<Todo> todos);
+        public static native TemplateInstance list(List<Todo> todos);
 
-    public static native TemplateInstance item(Todo todo);
-  }
-
-  @GET
-  public TemplateInstance list() {
-    return showList(Todo.listAll()).data("all", true);
-  }
-
-  @GET
-  @Path("/active")
-  public TemplateInstance active() {
-    return showList(Todo.listActive()).data("active", true);
-  }
-
-  @GET
-  @Path("/completed")
-  public TemplateInstance completed() {
-    return showList(Todo.listCompleted()).data("completed", true);
-  }
-
-  private TemplateInstance showList(List<Todo> todos) {
-    return Templates.list(todos).data("itemsLeft", Todo.countActive());
-  }
-
-  @POST
-  @Transactional
-  public Response add(@Form Todo todo, @HeaderParam("HX-Request") boolean hxRequest) {
-    Todo.persist(todo);
-    if (hxRequest) {
-      return Response.ok(Templates.item(todo)).header("HX-Trigger", "clear-add-todo").build();
+        public static native TemplateInstance item(Todo todo);
     }
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
 
-  @POST
-  @Path("{id}")
-  @Transactional
-  public Response edit(@PathParam("id") UUID id, @Form Todo todo) {
-    Todo dbTodo = Todo.findById(id);
-    dbTodo.title = todo.title;
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
-
-  @Transactional
-  @POST
-  @Path("/toggle-all")
-  public Response toggle() {
-    boolean allCompleted = Todo.countActive() == 0;
-    Todo.updateAllCompleted(!allCompleted);
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
-
-  @Transactional
-  @POST
-  @Path("/{id}/toggle")
-  public Object toggle(@PathParam("id") UUID id, @HeaderParam("HX-Request") boolean hxRequest) {
-    Todo todo = Todo.findById(id);
-    todo.completed = !todo.completed;
-    if (hxRequest) {
-      return Templates.item(todo);
+    @GET
+    public TemplateInstance list() {
+        return showList(Todo.listAll()).data("all", true);
     }
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
 
-  @POST
-  @Path("{id}/delete")
-  @Transactional
-  public Response delete(@PathParam("id") UUID id, @HeaderParam("HX-Request") boolean hxRequest) {
-    Todo.deleteById(id);
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
+    @GET
+    @Path("/active")
+    public TemplateInstance active() {
+        return showList(Todo.listActive()).data("active", true);
+    }
 
-  @POST
-  @Path("/clear-completed")
-  @Transactional
-  public Response deleteCompleted() {
-    Todo.deleteCompleted();
-    return Response.status(Status.FOUND).header("Location", "/todos").build();
-  }
+    @GET
+    @Path("/completed")
+    public TemplateInstance completed() {
+        return showList(Todo.listCompleted()).data("completed", true);
+    }
+
+    private TemplateInstance showList(List<Todo> todos) {
+        return Templates.list(todos).data("itemsLeft", Todo.countActive());
+    }
+
+    @POST
+    @Transactional
+    public Response add(@Form Todo todo, @HeaderParam("HX-Request") boolean hxRequest) {
+        Todo.persist(todo);
+        if (hxRequest) {
+            return Response.ok(Templates.item(todo)).header("HX-Trigger", "clear-add-todo").build();
+        }
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
+
+    @POST
+    @Path("{id}")
+    @Transactional
+    public Response edit(@PathParam("id") UUID id, @Form Todo todo) {
+        Todo dbTodo = Todo.findById(id);
+        dbTodo.title = todo.title;
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
+
+    @Transactional
+    @POST
+    @Path("/toggle-all")
+    public Response toggle() {
+        boolean allCompleted = Todo.countActive() == 0;
+        Todo.updateAllCompleted(!allCompleted);
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
+
+    @Transactional
+    @POST
+    @Path("/{id}/toggle")
+    public Object toggle(@PathParam("id") UUID id, @HeaderParam("HX-Request") boolean hxRequest) {
+        Todo todo = Todo.findById(id);
+        todo.completed = !todo.completed;
+        if (hxRequest) {
+            return Templates.item(todo);
+        }
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
+
+    @POST
+    @Path("{id}/delete")
+    @Transactional
+    public Response delete(@PathParam("id") UUID id, @HeaderParam("HX-Request") boolean hxRequest) {
+        Todo.deleteById(id);
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
+
+    @POST
+    @Path("/clear-completed")
+    @Transactional
+    public Response deleteCompleted() {
+        Todo.deleteCompleted();
+        return Response.status(Status.FOUND).header("Location", "/todos").build();
+    }
 }
